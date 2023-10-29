9e939684492ac22edc87a3a4f424faff52ed4744
YaredAlex <yareda25@gmail.com>
2023-10-29 09:52:02 +0530

    change to Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import MakePool from "./pages/MakePool";
 import { Chart as ChartJS } from "chart.js/auto";
@@ -8,9 +8,10 @@ import PoolDetail from "./pages/PoolDetail";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/LitendSurvey" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/make-pool" element={<MakePool />} />
           <Route path="/LitendSurvey/make-pool" element={<MakePool />} />
           <Route path="/pool-question/:qId" element={<QuestionView />} />
@@ -21,7 +22,7 @@ function App() {
           <Route path="/get-detail/:id" element={<PoolDetail />} />
           <Route path="/LitendSurvey/get-detail/:id" element={<PoolDetail />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
