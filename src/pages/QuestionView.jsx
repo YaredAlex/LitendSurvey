7e1e31f3d9b64484296d880af7322aacabaf1f35
YaredAlex <yareda25@gmail.com>
2023-11-13 15:16:10 +0530

    modifying question container

--- src/pages/QuestionView.jsx
+++ src/pages/QuestionView.jsx
@@ -14,24 +14,29 @@ import swal from "sweetalert";
 import log from "../assets/log_img.jpeg";
 
 const QuestionView = () => {
+  const maxQuestion = 5;
   const { qId } = useParams();
   const [options, setOption] = useState([]);
-  const [title, setTitle] = useState("");
+  const [allQuestion, setAllQuestion] = useState([]);
   const [loading, setLoading] = useState(true);
   const [question, setQuestion] = useState([]);
   const [ans, setAns] = useState({});
   const [submited, setSubmited] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pagenum, setpageNum] = useState(0);
   useEffect(() => {
     if (localStorage.getItem(qId.toString())) {
       swal("Respose already submited!", "", "info");
     }
-
     const getQuestion = async () => {
       setLoading(true);
       const docRef = doc(db, "PoolQuestions", qId);
       const result = await getDoc(docRef);
+      let que;
       if (result.exists) {
-        setQuestion(result.data().question);
+        que = result.data().question;
+        setAllQuestion(result.data().question);
+        divideQuestion(que);
       } else {
         console.log(result);
       }
@@ -55,7 +60,23 @@ const QuestionView = () => {
         console.error("Error:", error);
       });
   }, []);
-
+  const divideQuestion = (que) => {
+    let numPage = que.length / maxQuestion;
+    setQuestion(que);
+    // if (numPage > 0) {
+    //   setQuestion(que.slice(0, maxQuestion));
+    //   setpageNum(numPage);
+    // } else {
+    //   setQuestion(que);
+    // }
+  };
+  const gotoNextPage = () => {
+    let questionStart = page * maxQuestion;
+    let questionEnd = questionStart + maxQuestion;
+    setQuestion(allQuestion.slice(questionStart, questionEnd));
+    setPage(page + 1);
+    setpageNum(pagenum - 1);
+  };
   const sendAns = () => {
     let flag = 0;
     question.forEach((que) => {
@@ -90,7 +111,16 @@ const QuestionView = () => {
   };
   return (
     <>
-      <div className="question-container d-flex justify-content-center align-items-center">
+      <div className="">
+        <div
+          className="question-container "
+          style={{
+            position: "fixed",
+            width: "100%",
+            height: "100%",
+            zIndex: 1,
+          }}
+        ></div>
         {loading && (
           <div
             className="d-flex justify-content-center align-items-center "
@@ -106,87 +136,124 @@ const QuestionView = () => {
             <p className="h5 bg-white p-3">Loading...</p>
           </div>
         )}
-        {!submited && (
-          <div className="border border-primary rounded question-card">
-            <p className="h4 text-center " style={{ color: "#7952b3" }}>
-              LITEND
-            </p>
-            <div className="mb-2">
-              <img
-                src={log}
-                alt="image"
-                style={{
-                  display: "block",
-                  width: "100%",
-                  height: "100px",
-                  objectFit: "contain",
-                }}
-              />
-            </div>
-            {question &&
-              question.map((item, key) => (
-                <div key={key} className="mt-2">
-                  <p className="h4 mb-0">
-                    {item.title}
-                    {"?"}
-                  </p>
-                  <hr className="mt-0 mb-3" />
-                  <div>
-                    {item.option.map((option, index) => (
-                      <div key={index} className="d-flex  gap-3">
-                        <input
-                          type="radio"
-                          id={`${option + key}`}
-                          name={item.title}
-                          value={option}
-                          onChange={(e) =>
-                            setAns({ ...ans, [item.title]: e.target.value })
-                          }
-                        />
-                        <label
-                          className="question-label"
-                          htmlFor={option + key}
-                        >
-                          {option}{" "}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ))}
 
-            <button
-              className="btn w-100  mt-3 mx-auto"
-              style={{
-                maxWidth: "300px",
-                fontWeight: "bold",
-                display: "block",
-                backgroundColor: "#7952b3",
-                color: "white",
-                fontSize: "18px",
-              }}
-              onClick={() => sendAns()}
+        {!submited && (
+          <div
+            className=" d-flex mx-auto justify-content-center pt-3 align-items-center"
+            style={{
+              maxWidth: "500px",
+              minHeight: "100vh",
+              zIndex: 100,
+              position: "relative",
+            }}
+          >
+            <div
+              className="border border-primary rounded question-card"
+              style={{ width: "100%" }}
             >
-              Submit
-            </button>
+              <p className="h4 text-center " style={{ color: "#7952b3" }}>
+                LITEND
+              </p>
+              <div className="mb-2">
+                <img
+                  src={log}
+                  alt="image"
+                  style={{
+                    display: "block",
+                    width: "100%",
+                    height: "100px",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+              {question &&
+                question.map((item, key) => (
+                  <div key={key} className="mt-2">
+                    <p className="h4 mb-0">
+                      {key + 1 + ". "}
+                      {item.title}
+                      {"?"}
+                    </p>
+                    <hr className="mt-0 mb-3" />
+                    <div>
+                      {item.option.map((option, index) => (
+                        <div key={index} className="d-flex  gap-3">
+                          <input
+                            type="radio"
+                            id={`${option + key}`}
+                            name={item.title}
+                            value={option}
+                            onChange={(e) =>
+                              setAns({ ...ans, [item.title]: e.target.value })
+                            }
+                          />
+                          <label
+                            className="question-label"
+                            htmlFor={option + key}
+                          >
+                            {option}{" "}
+                          </label>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              {pagenum > 1 ? (
+                <button
+                  className="btn w-100  mt-3 mx-auto"
+                  style={{
+                    maxWidth: "300px",
+                    fontWeight: "bold",
+                    display: "block",
+                    backgroundColor: "#7952b3",
+                    color: "white",
+                    fontSize: "18px",
+                  }}
+                  onClick={() => gotoNextPage()}
+                >
+                  Next
+                </button>
+              ) : (
+                <button
+                  className="btn w-100  mt-3 mx-auto"
+                  style={{
+                    maxWidth: "300px",
+                    fontWeight: "bold",
+                    display: "block",
+                    backgroundColor: "#7952b3",
+                    color: "white",
+                    fontSize: "18px",
+                  }}
+                  onClick={() => sendAns()}
+                >
+                  Submit
+                </button>
+              )}
+            </div>
           </div>
         )}
+
         {submited && (
-          <div className="border border-primary question-card">
-            <p className="h6 text-center text-primary">LITEND</p>
-            <div className="mb-2">
-              <img
-                src={log}
-                alt="image"
-                style={{
-                  display: "block",
-                  width: "100%",
-                  height: "100px",
-                  objectFit: "contain",
-                }}
-              />
+          <div
+            className="d-flex  align-items-center justify-content-center"
+            style={{ zIndex: 50, position: "relative", height: "100vh" }}
+          >
+            <div className="border border-primary question-card rounded ">
+              <p className="h6 text-center text-primary">LITEND</p>
+              <div className="mb-2">
+                <img
+                  src={log}
+                  alt="image"
+                  style={{
+                    display: "block",
+                    width: "100%",
+                    height: "100px",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+              <p className="h5 text-center">Thank you!</p>
             </div>
-            <p className="h5 text-center">Thank you!</p>
           </div>
         )}
       </div>
